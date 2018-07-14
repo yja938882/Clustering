28386b7db05601bc7b4284ae0fad7c87ddeda4b3
연주안 <yeonjuan93@naver.com>
2018-07-14 19:40:36 +0900

    comment

--- k_means/k_means.js
+++ k_means/k_means.js
@@ -5,7 +5,7 @@
         attr_sz;
 
     /**
-     * @brief Constructor 
+     * Constructor 
      * @param {Object} args
      */
     function K_Means(args) {
@@ -17,18 +17,17 @@
     /**
      * @param {Object} args
      */
-    function init(args){
-
+    function init(args) {
         clusters = [];
 
-        for(var i = 0, len = data.length; i < len; i++){
+        for (var i = 0, len = data.length; i < len; i++) {
             attr_sz = data[i].length;
             data[i].cluster = -1;
         }
     
-        for(var i = 0, dsz = data.length; i < k; ){
+        for (var i = 0, dsz = data.length; i < k; ) {
             var _i = ((Math.random() * (dsz - 1)) | 0);
-            if(data[_i].cluster !== -1){
+            if (data[_i].cluster !== -1) {
                 continue;
             }
             clusters[i] = data[_i];
@@ -38,73 +37,84 @@
     }
 
     /**
-     * @breif 각 포인트들을 가장 가까운 클러스터에 할당.
+     * 각 포인트들을 가장 가까운 클러스터에 할당.
      * @param {Function} distfunc 두 포인트 간의 거리 계산을 위한 fucntion.
      * @param {Function} callback 할당 후 호출.
      */
-    K_Means.prototype.setNearestCluster = function(distfunc, callback){
-
+    K_Means.prototype.setNearestCluster = function(distfunc, callback) {
         var end = true;
-        for(var i = 0, len = data.length; i < len; i++){
-            for(var j = 0; j < k; j++){
-                if(data[i].cluster === -1 || 
+
+        for (var i = 0, len = data.length; i < len; i++) {
+            for (var j = 0; j < k; j++) {
+                if (data[i].cluster === -1 || 
                     distfunc( data[i], clusters[j] ) < distfunc( data[i], clusters[ data[i].cluster ])){
                     data[i].cluster = j;
                     end = false;
                 }
             }
         }
-        if( typeof callback == "function" ){
+
+        if (typeof callback == "function"){
             callback( data, clusters );
         }
         return end;
     }
 
     /**
-     * @breif 각 클러스터의 Centroid 재 설정.
+     * 각 클러스터의 Centroid 재설정.
      * @param {Function} callback 설정 후 호출.  
      */
-    K_Means.prototype.updateCentroid = function (callback){
-     
+    K_Means.prototype.updateCentroid = function(callback) {
         var centroids = [],
             cnt = [];
-        for(var i = 0, len = data.length; i < len; i++){
-            if( !centroids[ data[i].cluster ] ){
+
+        for (var i = 0, len = data.length; i < len; i++) {
+            if(!centroids[ data[i].cluster ]){
                 centroids[ data[i].cluster ] = [];
                 cnt[ data[i].cluster ] = 0;
             }
-            for(var a = 0; a < attr_sz; a++){
-                if( !centroids[ data[i].cluster ][a] ){
-                    centroids[ data[i].cluster  ][a] = 0;
+            for (var a = 0; a < attr_sz; a++) {
+                if( !centroids[data[i].cluster][a] ){
+                    centroids[data[i].cluster][a] = 0;
                 }
-                centroids[ data[i].cluster ][a] += data[i][a];
+                centroids[data[i].cluster][a] += data[i][a];
             }
-            cnt[ data[i].cluster ] ++;
+            cnt[data[i].cluster] ++;
         }
-        for(var i = 0; i < k; i++){
-            for(var a = 0; a < attr_sz; a++){
+
+        for (var i = 0; i < k; i++) {
+            for (var a = 0; a < attr_sz; a++) {
                 centroids[i][a] /= cnt[i];
             }
             clusters[i] = centroids[i];
             clusters[i].id = i;
         }
 
-        if( typeof callback == "function" )
-            callback( data, clusters );
+        if (typeof callback == "function")
+            callback(data, clusters);
     }
 
     /**
-     * @breif 클러스터링.
+     * 클러스터링.
+     * @param {Function} distfunc 두 포인트 간의 거리 계산을 위한 function.
+     * @param {Function} callback1 
+     * @param {Function} callback2
+     * @param {Function} callback3
      */
-	K_Means.prototype.clustering = function(distfunc, callback1, callback2, callback3) {
-        while( !set_nearest_cluster(distfunc, callback1) ){
+    K_Means.prototype.clustering = function(distfunc, callback1, callback2, callback3) {
+        while (!set_nearest_cluster(distfunc, callback1)) {
             updateCentroid(callback2);
         }
-        if( typeof callback3 == "function" )
+        if (typeof callback3 == "function")
             callback3(data ,clusters);
-	};
-
-    K_Means.prototype.euclidean = function(a, b){
+    };
+    /**
+     * Euclidean distance
+     * @param {Array<number>} a
+     * @param {Array<number>} b 
+     * @returns 포인트 a, b 간의 Euclidean distance.
+     */
+    K_Means.prototype.euclidean = function(a, b) {
         var d = [],
             ret = 0;
         for(var i = 0, len = a.length; i < len; i++){
@@ -116,8 +126,13 @@
         return Math.sqrt(ret);
     }
 
-
-    K_Means.prototype.manhattan = function(a, b){
+    /**
+     * Manhattan distance
+     * @param {Array<number>} a
+     * @param {Array<number>} b
+     * @returns 포인트 a, b 간의 Manhattan distance.
+     */
+    K_Means.prototype.manhattan = function(a, b) {
         var d = [],
             ret = 0;
         for(var i = 0, len = a.length; i < len; i++){
